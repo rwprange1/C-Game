8eb8b4d60546b904a3938b83b064a7e924178f63
rwprange1 <richwp12@gmail.com>
2025-06-09 19:49:08 -0400

    first commit

---
+++ Game.h
@@ -0,0 +1,13 @@
+/**
+ * The header file for our game class we will definbe the structure and the methods here
+ */
+
+#pragma once
+
+class Game {
+
+    public:
+        Game();
+        virtual ~Game();
+};
+
